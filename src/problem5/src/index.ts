e71023f5a842e471b06131d6de91854146ccef28
Vherniel Lebis <vherniel11@gmail.com>
2025-11-21 19:25:11 +0800

    ✨ feat(api): add POST /users endpoint to create new user

--- src/problem5/src/index.ts
+++ src/problem5/src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express, { type Request, type Response } from 'express';
-import { getUserById, getUsers } from './db/query/users';
+import { createUser, getUserById, getUsers } from './db/query/users';
 import { httpError } from './utils/http-error';
 import { corsMiddleware, verifyDbReady } from './middleware';
 
@@ -20,6 +20,25 @@ app.get('/users/:id', async (req: Request, res: Response) => {
 	const { id } = req.params;
 	try {
 		res.json(await getUserById(Number(id)));
+	} catch (error) {
+		httpError(res, 500);
+	}
+});
+
+// Create a new user
+app.post('/users', express.json(), async (req: Request, res: Response) => {
+	const { name, age, email } = req.body;
+
+	[name, age, email].forEach((field) => {
+		if (!field) {
+			return res
+				.status(400)
+				.json({ error: `Missing required field/s: ${field}` });
+		}
+	});
+
+	try {
+		res.status(201).json(await createUser({ name, age, email }));
 	} catch (error) {
 		httpError(res, 500);
 	}
