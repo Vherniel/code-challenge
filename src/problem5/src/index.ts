414c0f13bdfcb97d2f536e2631f7305b568097f4
Vherniel Lebis <vherniel11@gmail.com>
2025-11-21 19:22:54 +0800

    ✨ feat(api): add GET /users/:id endpoint to retrieve a single user

--- src/problem5/src/index.ts
+++ src/problem5/src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express, { type Request, type Response } from 'express';
-import { getUsers } from './db/query/users';
+import { getUserById, getUsers } from './db/query/users';
+import { httpError } from './utils/http-error';
 import { corsMiddleware, verifyDbReady } from './middleware';
 
 const app = express();
@@ -12,6 +13,16 @@ app.use(verifyDbReady);
 // Get all users
 app.get('/users', async (req: Request, res: Response) => {
 	res.json(await getUsers());
+});
+
+// Get user by ID
+app.get('/users/:id', async (req: Request, res: Response) => {
+	const { id } = req.params;
+	try {
+		res.json(await getUserById(Number(id)));
+	} catch (error) {
+		httpError(res, 500);
+	}
 });
 
 // Start the server
